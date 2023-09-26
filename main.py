00d5acbf7b8bc0a244b468ebb00c2c2046953c39
Renan Siqueira Antonio <renan.siqu@gmail.com>
2023-09-26 19:04:24 -0300

    feat: Change the main code for memory safe and fast detection with resize image

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import logging
 import time
+import gc
+
 from concurrent.futures import ProcessPoolExecutor
 
 import cv2
@@ -10,10 +12,17 @@ import face_recognition
 from src.config import settings
 
 
-print('\n\t\t', dlib.__version__)
-print('\t\t', dlib.DLIB_USE_CUDA, '\n')
+print('\n\t\tDlib Version:', dlib.__version__)
+print('\t\t GPU AVAILABLE:', dlib.DLIB_USE_CUDA, '\n')
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+def image_paths_generator(src_dir):
+    for root, _, files in os.walk(src_dir):
+        for file in files:
+            if file.lower().endswith(('.png', '.jpg', '.jpeg')):
+                yield os.path.join(root, file)
 
 
 def create_directories(paths_list):
@@ -21,11 +30,19 @@ def create_directories(paths_list):
         os.makedirs(path, exist_ok=True)
 
 
-def detect_faces(image_path):
+def detect_faces(image_path, resize_scale):
     logging.info(f"Detecting faces in the image: {image_path}")
+    
     image = cv2.imread(image_path)
+    
+    if resize_scale:
+        image = cv2.resize(image, (int(image.shape[1] * resize_scale), int(image.shape[0] * resize_scale)))
+    
     rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     face_locations = face_recognition.face_locations(rgb_image, model="cnn")
+    del rgb_image
+    gc.collect()
+    
     return image, face_locations
 
 
@@ -66,49 +83,54 @@ def save_faces(image, face_locations, save_path):
         cv2.imwrite(new_save_path, face_image)
 
 
-def process_image(src_path, dest_dir, src_dir):
+def process_image(src_path, dest_dir, src_dir, resize_scale):
     start_time = time.time()
 
     dest_path = os.path.join(dest_dir, os.path.relpath(src_path, src_dir))
     dest_folder = os.path.dirname(dest_path)
-    if not os.path.exists(dest_folder):
-        os.makedirs(dest_folder)
+    os.makedirs(dest_folder, exist_ok=True)
 
-    image, face_locations = detect_faces(src_path)
+    image, face_locations = detect_faces(src_path, resize_scale)
+
+    if face_locations and resize_scale:
+        face_locations = [(int(top / resize_scale), int(right / resize_scale), int(bottom / resize_scale), int(left / resize_scale)) for (top, right, bottom, left) in face_locations]
+        del image
+        gc.collect()
+        image = cv2.imread(src_path)
 
     if face_locations:
         save_faces(image, face_locations, dest_path)
         elapsed_time = time.time() - start_time
         logging.info(f"\tImage {src_path} processed in {elapsed_time:.2f} seconds.")
 
+    del image, face_locations
+    gc.collect()
 
-def process_directory(src_dir, dest_dir, max_workers=None):
-    if not os.path.exists(dest_dir):
-        os.makedirs(dest_dir)
 
-    image_paths = [os.path.join(root, file) 
-                   for root, _, files in os.walk(src_dir) 
-                   for file in files 
-                   if file.lower().endswith(('.png', '.jpg', '.jpeg'))]
+def process_directory(src_dir, dest_dir, max_workers, resize_scale):
+    os.makedirs(dest_dir, exist_ok=True)
 
     logging.info(f"Processing directory: {src_dir}")
+    logging.info(f"Running with {max_workers} simultaneous processes.")
 
-    if max_workers:
-        logging.info(f"Running with {max_workers} simultaneous processes.")
-
+    paths_generator = image_paths_generator(src_dir)
+    
     with ProcessPoolExecutor(max_workers=max_workers) as executor:
-        executor.map(process_image, image_paths, [dest_dir]*len(image_paths), [src_dir]*len(image_paths))
+        for image_path in paths_generator:
+            executor.submit(process_image, image_path, dest_dir, src_dir, resize_scale)
 
 
 if __name__ == '__main__':
 
     logging.info(f"\nNumber of available CPUs: {os.cpu_count()}\n")
+    resize_scale = settings.RESIZE_SCALE
+    max_workers = settings.MAX_WORKERS
 
     if settings.IS_TEST:
         directories = ['test/origin', 'test/destiny']
         create_directories(directories)
-        process_directory(directories[0], directories[1])
+        process_directory(directories[0], directories[1], max_workers, resize_scale=resize_scale)
     else:
-        process_directory(settings.PATH_ORIGIN, settings.PATH_DESTINY)
+        process_directory(settings.PATH_ORIGIN, settings.PATH_DESTINY, max_workers, resize_scale=resize_scale)
         
     logging.info("\nProcessing Finished!")
--- src/config/settings.example.py
+++ src/config/settings.example.py
@@ -2,3 +2,6 @@ PATH_ORIGIN = '__your_origin_path_that_contains_others_directories__'
 PATH_DESTINY = '__your_destiny_path__'
 
 IS_TEST = False
+
+RESIZE_SCALE = 0.5 # Reduce image size for fast detection
+MAX_WORKERS = None # If None, use all CPUs available
